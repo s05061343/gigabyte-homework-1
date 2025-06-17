580b3e835754225382012c3ca1bd79bdf3c18312
tom liu <s05061343@gmail.com>
2025-06-17 13:20:12 +0800

    整理

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers();
 builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
 {
     options.UseInMemoryDatabase("ProductDb");
